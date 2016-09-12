f183265cc3342a3abda6fb2bdf36c0f4088165a8
Luka Kvavilashvili <lukakvavilashvili@gmail.com>
2016-09-12 13:41:50 -0400

    Created functions for JSON response generation

---
+++ json.php
@@ -0,0 +1,27 @@
+<?php
+
+// function for returing successful response status
+function succeed($message) {
+    if (!$message) $message = "Success";
+    
+    $json = [
+        "success" => true,
+        "message" = >$message,
+    ];
+    
+    $jsonString = json_encode($json);
+    die($jsonString);
+}
+
+// function for returning failed response status
+function fail($message) {
+    if (!$message) $message = "Server failed to perform that operation";
+    
+    $json = [
+        "success" => false,
+        "message" => $message,
+    ];
+    
+    $jsonString = json_encode($json);
+    die($jsonString);
+}
